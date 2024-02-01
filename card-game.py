49ec591f1e6370d06941fb4fab9a21f91f14ee71
Em1L777 <emilfaizullov@gmail.com>
2024-02-01 14:33:00 +0100

    first

---
+++ card-game.py
@@ -0,0 +1,51 @@
+import random 
+
+
+class Card:
+    values = [1,2,3,4,5,6,7,8,9,10]
+
+class Player(Card):
+    def take(self):
+        pass
+    
+class Player1(Player):
+    def take1(self):
+        return random.choice(self.values)
+    
+class Player2(Player):
+    def take2(self):
+        return random.choice(self.values)
+
+p1 = Player1()
+result1 = p1.take1()
+
+p2 = Player2()
+result2 = p2.take2()
+
+print("Player1 take card (take/no)")
+answer1 = input()
+if answer1 == "take":
+    print(f"Your card number: {result1}")
+else:
+    print("game over")
+    exit()
+
+print("Player2 take card (take/no)")
+answer2 = input()
+if answer2 == "take":
+    print(f"Your card number:{result2}")
+else:
+    print("game over")
+    exit()
+
+if result1 > result2:
+    print("Player1 won, congratulations!")
+else:
+    print("Player2 won, congratulations!")
+
+
+
+
+
+
+
